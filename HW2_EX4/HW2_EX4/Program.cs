996e359a18bed6f0ce6d4d11cabc9163bb008aaf
Alexey Efimenko <Yogakuka@gmail.com>
2021-05-06 17:15:14 +0300

    Move ex5>ex4

--- HW2_EX4/HW2_EX4/Program.cs
+++ HW2_EX4/HW2_EX4/Program.cs
@@ -10,6 +10,40 @@ namespace HW2_EX4
     {
         static void Main(string[] args)
         {
+
+            int tries = 3;
+            string login = "root";
+            string password = "GeekBrains";
+            bool User = false;
+            string UsersLogin;
+            string UsersPassword;
+            do
+            {
+                Console.WriteLine("Ваш логин");
+                UsersLogin = Console.ReadLine();
+                Console.WriteLine("Пароль");
+                UsersPassword = Console.ReadLine();
+                if (UsersLogin == login & UsersPassword == password)
+                {
+
+                    User = true;
+                    tries = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Введен неправильный логин или пароль");
+                    Console.WriteLine("Попробуйте еще раз");
+                    tries--;
+                }
+            } while (tries > 0);
+            if (User)
+            {
+                Console.WriteLine("Вы прошли авторизацию");
+            }
+            else
+            {
+                Console.WriteLine("Превышено количество попыток для ввода пароля");
+            }
         }
     }
 }
